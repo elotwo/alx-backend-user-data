1becd66a09cc49b4c206306a7c304d4e12fcc9f9
elotwo <obiagwu94@gmail.com>
2024-11-08 00:29:22 +0100

    update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,27 +3,53 @@ import re
 import logging
 from typing import List
 
+
 def filter_datum(fields, redaction, message, separator):
+    """
+    this function  obfuscate some log massages
+    """
     parts = message.split(separator)
     for field in fields:
-        parts = [re.sub(f"{field}=[^;]*", f"{field}={redaction}", part) for part in parts]
+        parts = [
+                re.sub(f"{field}=[^;]*", f"{field}={redaction}", part)
+                for part in parts
+        ]
     return separator.join(parts)
 
+
 def get_logger():
+    """
+    this function returns a logger object
+    """
+    logging.getLogger(user_data)
+    logging.basicConfig(level=logging.INFO,)
+    return logger
 
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
     """
-    
+
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """
+        initiating class
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
-        
+
     def format(self, record: logging.LogRecord) -> str:
-        record.msg = filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        """
+        layout
+        """
+
+        record.msg = filter_datum(
+                self.fields,
+                self.REDACTION,
+                record.getMessage(),
+                self.SEPARATOR
+        )
         return super(RedactingFormatter, self).format(record)
