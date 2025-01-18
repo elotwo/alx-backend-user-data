135806bcd8900d2635120480a7249b18b1f7c5ad
elotwo <obiagwu94@gmail.com>
2025-01-18 17:31:53 +0100

    update

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,18 +21,6 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(401)
-def auth_error(error) -> str:
-    """ handler for 401"""
-    return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def auth_forbidden(error) -> str:
-    """ handler for 403"""
-    return jsonify({"error": "Forbidden"}), 403
-
-
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
